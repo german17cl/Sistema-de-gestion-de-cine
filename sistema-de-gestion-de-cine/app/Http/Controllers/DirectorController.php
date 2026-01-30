d0f62025737787d4fee350b3c79b572c0b0fa1bb
german17cl <germancamp17@gmail.com>
2026-01-30 09:56:23 +0100

    Controller de director

--- sistema-de-gestion-de-cine/app/Http/Controllers/DirectorController.php
+++ sistema-de-gestion-de-cine/app/Http/Controllers/DirectorController.php
@@ -4,34 +4,108 @@ namespace App\Http\Controllers;
 
 use App\Models\Director;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use App\Models\Movie;
 
 class DirectorController extends Controller
 {
+    // Solo usuarios autenticados
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Listado de directores
+     */
     public function index()
     {
-        return Director::all();
+        $directors = Director::latest()->paginate(10);
+
+        return view('directors.index', compact('directors'));
     }
 
+    /**
+     * Formulario crear
+     */
+    public function create()
+    {
+        return view('directors.create');
+    }
+
+    /**
+     * Guardar director
+     */
     public function store(Request $request)
     {
-        return Director::create($request->all());
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'birth_date' => 'nullable|date',
+            'nationality' => 'nullable|string|max:100',
+            'biography' => 'nullable|string',
+        ]);
+
+        Director::create($data);
+
+        return redirect()
+            ->route('directors.index')
+            ->with('success', 'Director creado correctamente');
     }
 
+    /**
+     * Ver detalle
+     */
     public function show(Director $director)
     {
-        return $director->load('movies');
+        $director->load('movies');
+
+        return view('directors.show', compact('director'));
     }
 
+    /**
+     * Formulario editar
+     */
+    public function edit(Director $director)
+    {
+        return view('directors.edit', compact('director'));
+    }
+
+    /**
+     * Actualizar director
+     */
     public function update(Request $request, Director $director)
     {
-        $director->update($request->all());
-        return $director;
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'birth_date' => 'nullable|date',
+            'nationality' => 'nullable|string|max:100',
+            'biography' => 'nullable|string',
+        ]);
+
+        $director->update($data);
+
+        return redirect()
+            ->route('directors.index')
+            ->with('success', 'Director actualizado correctamente');
     }
 
+    /**
+     * Vista confirmar borrado
+     */
+    public function delete(Director $director)
+    {
+        return view('directors.delete', compact('director'));
+    }
+
+    /**
+     * Eliminar director
+     */
     public function destroy(Director $director)
     {
         $director->delete();
-        return response()->noContent();
+
+        return redirect()
+            ->route('directors.index')
+            ->with('success', 'Director eliminado correctamente');
     }
 }
